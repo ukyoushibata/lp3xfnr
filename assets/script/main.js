25d6eb8c54e6d9420eee8970cfbd090e327176fe
ukyou.shibata <xfnr9710@yahoo.co.jp>
2024-08-19 06:01:09 +0900

    8月19日

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,22 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.voice__slider').slick({
         dots: true,
         arrows: true,
-        infinite: false,
-        speed: 500,
+        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
+        centerMode: true,
         responsive: [{
-        breakpoint: 768,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: true,
-            arrows: true,
-            centerMode: true,
-            centerPadding: "10%",
-        }
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows: true,
+                centerMode: true,
+                centerPadding: "10%",
+            }
         }]
     });
 });
